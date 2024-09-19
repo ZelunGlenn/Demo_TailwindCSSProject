baf0f3da341ddd1998ac66b7cadfd46cc403b302
Zelun Zhang <121286574+ZelunGlenn@users.noreply.github.com>
2024-09-19 14:13:42 +0930

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "Strong-Cyan": "hsl(171, 66%, 44%)",
+        "Light-Blue": "hsl(233, 100%, 69%)"
+      },
+      fontFamily: {
+        'Bai-Jamjuree': ['"Bai Jamjuree"', 'sans-serif'],
+      },
+      screens: {
+        "sm": "475px",
+        "lg": "1440px",
+      },
+      spacing: {
+        "375": "23.44rem",
+        "1440": "90rem",
+      },
+      backgroundImage: {
+        "header-desktop": "url('./images/bg-header-desktop.png')",
+        "header-mobile": "url('./images/bg-header-mobile.png')"
+      }
+    },
+  },
+  plugins: [],
+}
+
